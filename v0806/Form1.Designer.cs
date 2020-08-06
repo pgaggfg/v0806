bf59ad683fdde7aeae4021379db2899a74679de6
Citrus.T <datdaisukemikancha@gmail.com>
2020-08-06 10:51:56 +0900

    演習8-4

--- v0806/Form1.Designer.cs
+++ v0806/Form1.Designer.cs
@@ -36,26 +36,29 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(131, 120);
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(29, 49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.Size = new System.Drawing.Size(118, 21);
             this.label1.TabIndex = 0;
-            this.label1.Text = "(´ﾟдﾟ｀)";
+            this.label1.Text = "┏(廿_廿 )┛";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // timer1
             // 
             this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(236, 221);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -10,9 +10,11 @@ using System.Windows.Forms;
 
 namespace v0806
 {
-    public partial class Form1 : Form;
-    int vx,vy;
+    public partial class Form1 : Form
     {
+        int vx = -10;
+        int vy = -10;
+        String kao = "┏(廿_廿 )┛";
         public Form1()
         {
             InitializeComponent();
@@ -20,9 +22,40 @@ namespace v0806
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-             vx = 10;
-             vy = 10;
+            label1.Left += vx;
+            label1.Top += vy;
 
+            if (label1.Left < 0)
+            {
+                vx = 10;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 10;
+            }
+            if (label1.Left > 236)
+            {
+                vx = -10;
+            }
+            if (label1.Top > 221)
+            {
+                vy = -10;
+            }
+            string t = label1.Text;
+            label1.Text = kao;
+            kao = t;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            vx = 0;
+            vy = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("幅" + ClientSize.Width);
+            MessageBox.Show("高さ" + ClientSize.Height);
         }
     }
 }
